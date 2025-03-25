0838ce19c14fbcf790231de77aad60245482142b
Saskapult <thekieransmith@gmail.com>
2025-03-25 16:31:29 -0700

    giving up on apptainers

--- generation_job.sh
+++ generation_job.sh
@@ -2,6 +2,14 @@
 #SBATCH --time=00:15:00
 #SBATCH --account=def-someuser
 
+#SBATCH --nodes=1
+#SBATCH --gpus=p100:1
+#SBATCH --ntasks-per-node=24
+#SBATCH --exclusive
+#SBATCH --mem=2G
+#SBATCH --time=0-03:00
+#SBATCH --account=def-someuser
+
 # This job 
 
 INPUT="fema_nims_doctrine-2017.pdf"
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("filename")
 	parser.add_argument("-o", "--output")
+	# parser.add_argument("-m", "--model", default="")
 	args = parser.parse_args()
 
 	print(f"Reading {args.filename}")
@@ -15,8 +16,11 @@ def main():
 
 	print("Hello from graph-ingestion-playground!")
 
-	# pages = pdf.get_pdf_pages_text(args.filename)
-	# chunks = pdf.make_pdf_chunks(pages)
+	pages = pdf.get_pdf_pages_text(args.filename)
+	# TODO: Make it smarter 
+	# Maybe aggregate text until a threshold is reached, then attach sources to that 
+	# Not sure how to split it though, by sentences or words or characters? 
+	chunks = pdf.make_pdf_chunks(pages)
 	
 	# print("First three chunks")
 	# for c in chunks[:3]:
@@ -24,46 +28,21 @@ def main():
 	# 	print(c)
 	# print("--")
 
-	# for i, entry in enumerate(chunks):
-	# 	print(f"Process chunk {i+1}/{len(chunks)}")
-	# 	graph = None
+	kg = KGGen(
+		model=kg_gen_model,
+		api_key=os.getenv("OPENAI_API_KEY"),
+	)
 
-	# 	sources = entry["sources"]
-	# 	graph_json = graph.graph_to_json(graph)
+	for i, entry in enumerate(chunks):
+		print(f"Process chunk {i+1}/{len(chunks)}")
+		graph = kg.generate(
+			input_data=entry,
+		)
 
-	# 	file = args.output + f"/chunk-{sources}.json"
-	# 	print(f"\tSaving as '{file}'")
-	# 	graph.save_json(graph_json, file)
-		
-
-	# We get all of the entities and relation and we link them to this page
-
-	# A node is just an id 
-	# A relationship has a source (pdf page number)
-	# Relationship RELATION has [edge, page number]
-	
-	# 
-	# When answering 
-	# 
-	# Create KG 
-	# Find similar items in db 
-	# expand idk 
-	# 
-	# Have lsit of relations 
-	# 
-	# Get relations
-	# Find 
-	# I need to read a paper on this 
-	# 
-
-	# kg = KGGen(
-	# 	model=kg_gen_model,
-	# 	api_key=os.getenv("OPENAI_API_KEY"),
-	# )
-	# graph = kg.generate(
-	# 	input_data=entry["html"],
-	# 	output_folder="./scratch"
-	# )
+		graph_json = graph.graph_to_json(graph)
+		file = args.output + f"/chunk-{i}.json"
+		print(f"\tSaving as '{file}'")
+		graph.save_json(graph_json, file)
 
 
 if __name__ == "__main__":
